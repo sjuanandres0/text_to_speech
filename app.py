9ffdd2e8f527e35c57506523d2ec1a6a99a293a4
Juan Simonetti <sjuanandres0@gmail.com>
2022-12-03 16:25:51 +0100

    test vercel

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def index():
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
-
+    
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
---
+++ wsgi.py
@@ -0,0 +1,11 @@
+# import os, sys
+# sys.path.append(os.path.abspath(os.path.join(dirname(__file__), @..@)))
+
+# from application import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run()
+
+from app import app
