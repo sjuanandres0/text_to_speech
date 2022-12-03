0ada967f5a52e923e98dbdfff69f196c3c76f6a9
Juan Simonetti <sjuanandres0@gmail.com>
2022-12-03 17:03:21 +0100

    rollback vercel

--- index.py
+++ index.py
@@ -41,7 +41,7 @@ def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
     
     try:
-        db.session.delete(task_to_delete)
+        # db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
     except:
